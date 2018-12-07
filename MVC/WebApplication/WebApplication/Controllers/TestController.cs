646c005fbbf494b4cc0d2c224429c1d5aa231135
XFXJ <1454337412@qq.com>
2018-12-07 09:09:31 +0800

    454
    jk

---
+++ MVC/WebApplication/WebApplication/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "你是谁！";
+        }
+        public Customer getGustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "刘帅";
+            c.Address = "柳州职业技术学院";
+            return c;
+        }
+    }
+}
