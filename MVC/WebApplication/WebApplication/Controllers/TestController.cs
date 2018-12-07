f95bfc8443098bc4c2218bf45562e8eb273efb55
XFXJ <1454337412@qq.com>
2018-12-07 12:07:10 +0800

    2456

--- MVC/WebApplication/WebApplication/Controllers/TestController.cs
+++ MVC/WebApplication/WebApplication/Controllers/TestController.cs
@@ -20,5 +20,26 @@ namespace WebApplication.Controllers
             c.Address = "柳州职业技术学院";
             return c;
         }
+        public ActionResult GetView()
+        {
+            string greeting;
+            DateTime dt = DateTime.Now;
+            int h = dt.Hour;
+            if (h < 12)
+            {
+                greeting = "早上好";
+            }
+            else
+            {
+                greeting = "中午好";
+            }
+            ViewData["greeting"] = greeting;
+            Employee emp = new Employee();
+            emp.Name = "刘帅";
+            emp.Salary = 2000;
+            ViewData["Employee"] = emp;
+            return View("MyView");
+        }
     }
+
 }
