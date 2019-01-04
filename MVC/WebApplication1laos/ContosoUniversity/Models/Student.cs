3226155507214f4183dfd114adbd002e23b5ab1e
baozhenqin <baozhenqin2013@hotmail.com>
2019-01-04 12:15:04 +0800

    asd

--- MVC/WebApplication1laos/ContosoUniversity/Models/Student.cs
+++ MVC/WebApplication1laos/ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,12 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [Display(Name="姓名")]
         public string Name { get; set; }
-      
+      [Display(Name="注册日期")]
         public DateTime EnrollmentDate { get; set; }
-
+        [Display(Name="选课信息")]
+        public string Secret { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
