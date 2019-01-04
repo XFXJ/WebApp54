c668c11e4bec34983f2c9d4b72d4ef66049e16f8
XFXJ <1454337412@qq.com>
2019-01-04 20:42:55 +0800

    asd

--- MVC/WebApplication1laos/ContosoUniversity/Global.asax.cs
+++ MVC/WebApplication1laos/ContosoUniversity/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace ContosoUniversity
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -16,6 +17,7 @@ namespace ContosoUniversity
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+          
         }
     }
 }
