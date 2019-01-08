a8d8be730f817c4e0b2c0898fa973f2fa6cb4bab
baozhenqin <baozhenqin2013@hotmail.com>
2019-01-08 16:47:45 +0800

    a

---
+++ MVC/ContosoUniversity后台作业/ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+        [Display(Name = "注册日期")]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "选课信息")]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
