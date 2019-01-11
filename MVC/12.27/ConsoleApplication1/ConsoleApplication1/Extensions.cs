17aef30bcb84845b7cff1239088c7fa323fbefe7
baozhenqin <baozhenqin2013@hotmail.com>
2019-01-11 13:17:23 +0800

    s

---
+++ MVC/12.27/ConsoleApplication1/ConsoleApplication1/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+       (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+}
